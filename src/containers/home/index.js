c78e18d16dcafa22267758c998d5d69fcded735e
Web-Star <ben.lin0025@gmail.com>
2020-04-30 13:48:24 -0400

    RP-134: Add events group section and tiled component (#12)
    Added event group section and created tiled section component.

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { UpcomingSection } from './upcomingSection';
+import { EventsGroup } from './eventsGroupSection';
+import { upcomingEvents, trendingEvents } from '../../mocks/events';
+import { setEventTypeAction } from 'store/homepage';
 
 export const Home = () => {
+  const eventType = useSelector(({ homepageReducer }) => homepageReducer.eventType);
+
+  const dispatch = useDispatch();
+  const changeType = (type) => {
+    dispatch(setEventTypeAction(type));
+  };
+
   return (
-    <div>
-      <UpcomingSection />
+    <div
+      style={{
+        background: 'linear-gradient(197.56deg, #455FE5 -5.72%, #8245E5 37.62%, #FFFFFF 78.06%)',
+      }}
+    >
+      <EventsGroup
+        events={trendingEvents}
+        gutter={20}
+        changeType={changeType}
+        eventType={eventType}
+      />
+      <UpcomingSection events={upcomingEvents} />
     </div>
   );
 };
--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+
 import { loginAction } from 'store/auth';
 import LoginForm from './components/loginForm';
 
