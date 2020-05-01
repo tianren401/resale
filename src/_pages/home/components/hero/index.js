3e89f3e9543692f33b785804fe2d8f06e75874e2
StephenTHendrix <StephenTHendrix@gmail.com>
2020-05-01 12:16:44 -0500

    deleted images/test folders

--- src/_pages/home/components/hero/index.js
+++ src/_pages/home/components/hero/index.js
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from 'react';
 import { Carousel } from './carousel';
 import { get } from '_helpers'
 
-import broadwayEvent11 from '_images/test/broadwayEvent11.png';
-import broadwayEvent12 from '_images/test/broadwayEvent12.png';
-import broadwayEvent13 from '_images/test/broadwayEvent13.png';
-import broadwayEvent14 from '_images/test/broadwayEvent14.png';
+import broadwayEvent11 from '_images/mocks/broadwayEvent11.png';
+import broadwayEvent12 from '_images/mocks/broadwayEvent12.png';
+import broadwayEvent13 from '_images/mocks/broadwayEvent13.png';
+import broadwayEvent14 from '_images/mocks/broadwayEvent14.png';
 
 export const Hero = () => {
   const [events, setEvents] = useState([]);
--- src/_pages/home/components/hero/testData.js
+++ src/_pages/home/components/hero/testData.js
@@ -1,7 +1,7 @@
-import broadwayEvent11 from '_images/test/broadwayEvent11.png';
-import broadwayEvent12 from '_images/test/broadwayEvent12.png';
-import broadwayEvent13 from '_images/test/broadwayEvent13.png';
-import broadwayEvent14 from '_images/test/broadwayEvent14.png';
+import broadwayEvent11 from '_images/mocks/broadwayEvent11.png';
+import broadwayEvent12 from '_images/mocks/broadwayEvent12.png';
+import broadwayEvent13 from '_images/mocks/broadwayEvent13.png';
+import broadwayEvent14 from '_images/mocks/broadwayEvent14.png';
 
 export const testImages = [
   broadwayEvent11,
